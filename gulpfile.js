1e85da98a89e78305ec74b56b0b3a57fbabb4d1c
Mark Ryba <marklryba@Marks-MacBook-Pro-2.local>
2019-09-07 13:59:59 -0400

    Added plugins and modified Gulp File to add a test-build step to the workflow. Intended for building a page as static HTML/CSS using Pug and PostCSS en masse, then that processed code can be split into modules and made HubSpot friendly

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ const postcssImport = require('postcss-import');
 const autoprefixer = require('autoprefixer');
 const pxtorem = require('postcss-pxtorem');
 const mqpacker = require('css-mqpacker');
+const pug = require("gulp-pug");
 
 
 function css() {
@@ -36,7 +37,36 @@ function copy() {
         .pipe(dest('dist/'))
 }
 
+function testCss() {
+    return src('test/src/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([
+            postcssImport(),
+            pxtorem({
+                propList: ['*']
+            }),
+            mqpacker({
+                sort: true
+            }),
+            autoprefixer(),
+            precss()
+        ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('test/dist/'))
+}
+
+function testPug() {
+    return src('test/src/**/*.pug')
+        .pipe(pug({
+            doctype:'html',
+            pretty:true
+        }))
+        .pipe(dest('test/dist'))
+}
+
 exports.default = function() {
     watch('src/**/*.css', css);
-    watch('src/**/*', copy)
+    watch('src/**/*', copy);
+    watch('test/src/**/*.css', testCss);
+    watch('test/src/**/*.pug', testPug);
   };
